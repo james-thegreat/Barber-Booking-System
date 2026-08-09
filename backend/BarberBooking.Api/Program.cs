27bac0ad4dbe88bdc971a0d37298997f6b89b7f0
James Dunlop <jamezdunlop@gmail.com>
2026-08-10 10:32:15 +1200

    Configure password hashing

--- backend/BarberBooking.Api/Program.cs
+++ backend/BarberBooking.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using BarberBooking.Infrastructure.Data;
+using BarberBooking.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
 builder.Services.AddDbContext<BarberDbContext>(options =>
     options.UseSqlite("Data Source=barberbooking.db"));
